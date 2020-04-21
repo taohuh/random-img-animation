5761ba38d4492f06e31af60f1ad0efb6054242a7
taohuh <pimpakarn@maqe.com>
2020-04-21 10:07:40 +0700

    Add function for random image by index

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 
 const TOTAL_IMAGES = 15;
+const TOTAL_IMAGE_EL = 12;
 const RANDOM_TIME = 5000;
-const numberArr = [...Array(TOTAL_IMAGES).keys()].map(i => i + 1);
+const numberImgArr = [...Array(TOTAL_IMAGES).keys()].map(i => i + 1);
 
 const imagesEl = document.querySelectorAll('.grid-img');
+
+let isAnimate = true;
+
+function randomIndex(num) {
+	return Math.floor(Math.random() * num);
+}
 
 function shuffle(arr) {
 	let counter = arr.length;
 	let temp, index;
 
 	while (counter > 0) {
-		index = Math.floor((Math.random() * counter));
+		index = randomIndex(counter);
 		counter--;
 
 		temp = arr[counter];
@@ -21,20 +28,41 @@ function shuffle(arr) {
 	return arr;
 }
 
+function changeImgSrc(el, srcImg) {
+	el.src = `./img/${srcImg}.jpeg`;
+}
 
-function renderImage() {
-	shuffleArr = shuffle(numberArr);
+function generateGallery() {
+	shuffleArr = shuffle(numberImgArr);
 
 	imagesEl.forEach((imagesEl, index) => {
+		changeImgSrc(imagesEl, shuffleArr[index]);
 		imagesEl.classList.add('loaded');
-		imagesEl.style.transitionDelay = `${Math.floor(Math.random() * 550)}ms`;
-
-		imagesEl.src = `./img/${shuffleArr[index]}.jpeg`;
+		imagesEl.style.transitionDelay = `${Math.floor(Math.random() * 400)}ms`;
 	});
 }
 
-renderImage();
+function randomImage() {
+	const index = randomIndex(TOTAL_IMAGE_EL);
 
-// setInterval(() => {
-// 	renderImage();
-// }, RANDOM_TIME);
+	let isTransitionRemoved = true;
+
+	imagesEl[index].classList.remove('loaded');
+
+	imagesEl[index].addEventListener('transitionend', () => {
+		if (isTransitionRemoved) {
+			changeImgSrc(imagesEl[index], shuffle(numberImgArr)[index]);
+			imagesEl[index].classList.add('loaded');
+
+			isTransitionRemoved = false;
+		}
+	})
+}
+
+// TODO: Generate div by js
+
+generateGallery();
+
+setInterval(() => {
+	randomImage();
+}, RANDOM_TIME);
